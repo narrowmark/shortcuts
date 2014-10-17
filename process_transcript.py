561fc5919cefd8d8b15eab2c54dfc6c54b5829d7
Mireles <mireles@narrowmark.com>
2014-10-17 01:52:25 -0700

    - Refactored script to use functions for getting word lists and getting a dictionary of common words. - Changed objective from a set of words to a dictionary with words as keys and their counts as values. - Added pickling and rudimentary file existence check.

--- process_transcript.py
+++ process_transcript.py
@@ -1,30 +1,51 @@
-import re
+import re, pickle, os.path
 
-transcript = open('composite.txt')
-text = ''
+# Opens a text file and returns a list of its words, in lowercase.
+def get_word_list(source='composite.txt'):
+  if not os.path.isfile(source):
+    print "File does not exist"
+    exit()
 
-for line in transcript.readlines():
-  text += line
+  transcript = open(source)
+  text = ''
 
-def process(text):
-  output = re.findall(r"[\w']+", text)
-  return output
+  for line in transcript.readlines():
+    text += line
 
-text = process(text)
-text_set = set(text)
-common = []
+  transcript.close()
+  text = text.lower()
+  words = re.findall(r"[\w']+", text)
 
-print len(text_set)
+  return words
 
-ignored = 0
-for word in text_set:
-  if text.count(word) > 100:
-    common.append(word)
-    print word, len(common)
-  else:
-    ignored += 1
-    if ignored % 100 == 0:
-      print ignored, " ignored"
+# Takes a list of words
+# Returns a dictionary of words that appear more often than the given
+# lower bound together with their count in the corpus.
+def get_common_word_dict(words, low_bound=100):
+  common = {}
+  ignored = 0
+  word_set = set(words)
 
-print len(common)
-print common
+  for word in word_set:
+    count = words.count(word)
+    if count > low_bound:
+      common[word] = count
+    else:
+      ignored += 1
+      if ignored % 100 == 0:
+        print ignored, " ignored"
+
+  return common
+
+if __name__ == '__main__':
+  words = get_word_list()
+  common_words = get_common_word_dict(words)
+
+  print common_words
+
+  word_file = open('common_words', 'wb')
+  pickle.dump(common_words, word_file)
+  word_file.close()
+
+  word_file = pickle.load(open('common_words', 'rb'))
+  print word_file
